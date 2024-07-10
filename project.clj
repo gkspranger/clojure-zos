5fa568e79ac00f15643ba353799fe456bb695bfc
gkspranger <greg@spranger.us>
2024-07-10 16:17:56 -0400

    exclude jzos from jar

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject hello "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.ibm.jzos/ibmjzos "2.4.11.2"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot hello.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles {:uberjar
+             {:aot :all
+              :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :provided
+             {:dependencies [[com.ibm.jzos/ibmjzos "2.4.11.2"]]}})
