5759813d26fd8ab43ddac269d815a1b4e329f660
gkspranger <greg@spranger.us>
2024-07-10 10:52:00 -0400

    more clojurey

--- src/hello/core.clj
+++ src/hello/core.clj
@@ -1,25 +1,29 @@
 (ns hello.core
   (:gen-class))
 
-(defn doit []
-  (let [ds (com.ibm.jzos.ZFile.
-            "//'MYZUSER.TEST.PS'"
-            "rb,type=record,noseek")
-        recl (.getLrecl ds)
-        ba (byte-array recl)]
+(defn load-ds [dsn]
+  (let [ds (com.ibm.jzos.ZFile. dsn "rb,type=record,noseek")]
     (println (str "  file name: " (.getActualFilename ds)))
+    (println (str "      dsorg: " (.getDsorg ds)))
     (println (str "rec. format: " (.getRecfm ds)))
-    (println (str "rec. length: " recl))
+    (println (str "rec. length: " (.getLrecl ds)))
+    ds))
 
-    (while (not= -1 (.read ds ba))
-      (println (String. ba)))))
+(defn get-ds [file]
+  (let [dsn (str "//'" file "'")
+        dsn-exists (. com.ibm.jzos.ZFile dsExists dsn)]
+    (when dsn-exists
+      (load-ds dsn))))
+
+(defn print-ds [ds]
+  (when ds
+    (let [rec (byte-array (.getLrecl ds))]
+      (while (not= -1 (.read ds rec))
+        (println (String. rec))))))
 
 (defn -main
-  [& _args]
-  (doit))
-
-(comment
-  
-  (-main)
-  
-  )
+  [& args]
+  (when-let [file (first args)]
+    (if-let [ds (get-ds file)]
+      (print-ds ds)
+      (println (str file "was not found")))))
